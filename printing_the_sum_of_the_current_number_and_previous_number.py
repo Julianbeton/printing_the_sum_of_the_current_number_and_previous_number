cadeb759a2d6f8511bad484531eb426c0370a77d
Julianbeton <julianibeton@iskolarngbayan.pup.edu.ph>
2024-01-23 21:14:59 +0800

    uploading pseudocode

--- printing_the_sum_of_the_current_number_and_previous_number.py
+++ printing_the_sum_of_the_current_number_and_previous_number.py
@@ -1,3 +1,13 @@
 # Exercise 2: Print the sum of the current number and the previous number
 
 # Write a program to iterate the first 10 numbers, and in each iteration, print the sum of the current and previous number.
+
+# pseudocode
+# printing what program run
+
+# setting value for first previous number
+
+# range for the first 10 numbers
+
+# printing current number value, previous number value, and the sum value
+
