d922e9b3e55283872fb41cd70959d5b55cf1747a
Julianbeton <julianibeton@iskolarngbayan.pup.edu.ph>
2024-01-23 21:29:40 +0800

    uploading final code

--- printing_the_sum_of_the_current_number_and_previous_number.py
+++ printing_the_sum_of_the_current_number_and_previous_number.py
@@ -4,17 +4,18 @@
 
 # pseudocode
 # printing what program run
-print("Printing current and previous number and their sum in a range(10)")
+print("\033[1;32;40mPrinting current and previous number and their sum in a range(10)")
+
 # setting value for first previous number
 previous_number = 0
 
 # range for the first 10 numbers
-for i in range (1,10):
+for i in range (0,10):
     sum_of_the_current_number_and_previous_number = previous_number + i
-    print("Current Number", i, "Previous Number ", previous_number, " Sum: ",sum_of_the_current_number_and_previous_number)
-    # modify previous number
-    # set it to the current number
+# printing current number value, previous number value, and the sum value    
+    print("\033[1;32;40mCurrent Number is", i, "Previous Number is ", previous_number, " Sum: ",sum_of_the_current_number_and_previous_number)
+    
     previous_number = i
-# printing current number value, previous number value, and the sum value
+
     
     
